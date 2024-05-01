47bb0a318c2debca8a753e90873cb666685915bf
kelvinlam1986 <kelvinlam_1986@yahoo.com.vn>
2024-04-30 22:39:18 -0700

    Update Update Auction Consumer And Delete Auction Consumer

--- AuctionService/Controllers/AuctionsController.cs
+++ AuctionService/Controllers/AuctionsController.cs
@@ -96,6 +96,8 @@ namespace AuctionService.Controllers
             auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
             auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
 
+            await _publishEndpoint.Publish(_mapper.Map<AuctionUpdated>(auction));
+
             var result = await _context.SaveChangesAsync() > 0;
             if (!result)
             {
@@ -116,6 +118,12 @@ namespace AuctionService.Controllers
 
             //TODO: Check seller == username
             _context.Auctions.Remove(auction);
+
+            await _publishEndpoint.Publish<AuctionDeleted>(new AuctionDeleted
+            {
+                Id = auction.Id.ToString()
+            });
+
             var result = await _context.SaveChangesAsync() > 0;
             if (!result)
             {
